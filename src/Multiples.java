0526f3db726b6cac381346add1c72000e04f0bf6
Emmett <180994452+eruss341@users.noreply.github.com>
2024-09-09 14:38:11 -0400

    Created Multiples to find all multiples of 3 and 5 below 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+
+    public static void main(String[] args) {
+
+        int num = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                num++;
+            }
+        }
+        System.out.println(num);
+    }
+
 }
+
